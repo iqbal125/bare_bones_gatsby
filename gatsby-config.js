249a18792ff9e77fe235ddf3ac148315bc79b3d1
iqbal125 <iqbal125@yahoo.com>
2020-07-22 20:26:25 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    defaultTitle: `Gatsby Startup/Hackathon Starter`,
+    defaultDescription: `A starter for building a startup or hackathon project`,
+    siteUrl: `https://example.com`,
+    defaultImage: "./static/favicon.ico",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-
+import "./styles/global.css"
 import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Default Page</h1>
   </Layout>
 )
 
